4ae599c85b54fa4fbe3ef8bd853c157608f34788
LZP1987 <516264135@qq.com>
2020-07-25 12:23:22 +0800

    上传主程序test_case_helper.py

--- src/test_case_helper.py
+++ src/test_case_helper.py
@@ -1,4 +1,11 @@
-# 爬取一个html并保存
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:三哥
+@email:516264135@qq.com
+@file:execute_shell.py
+@time:2020/07/24
+"""
 
 import modules.ui.ui_entrance
 
